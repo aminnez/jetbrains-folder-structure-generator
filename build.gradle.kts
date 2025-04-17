58fa264813d4318b5bf4fa7882265f4c5219e496
Amin Nezampour <aminnezam@gmail.com>
2025-04-17 14:06:51 +0330

    fix: version compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.aminnez.plugin.clean"
-version = "1.2"
+version = "1.3"
 
 repositories {
     mavenCentral()
@@ -31,7 +31,7 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild = provider { "221" }
+        sinceBuild = provider { "231" }
         untilBuild = provider { null }
     }
 
